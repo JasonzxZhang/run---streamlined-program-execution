f5315ba92f62469931ba83e352ebd06601c5986b
jasonzxzhang <zuxingz@gmail.com>
2017-11-23 22:33:14 -0500

    fixed config_path not found error

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 new_version='1.0.0'
-config_path='~/.runconfig'
-bash_profile_path='~/.bash_profile'
-run_path='/usr/local/bin/run'
+config_path=~/.runconfig
+bash_profile_path=~/.bash_profile
+run_path=/usr/local/bin/run
 
 # Compile to UNIX executable and move to /usr/local/lbin
 install_RUN() {
@@ -13,26 +13,40 @@ install_RUN() {
 	cp run '/usr/local/bin/'
 
 	# Add "run" alias to bash_profile
-	echo "alias run='/usr/local/bin/run'" >> bash_profile_path
-	source bash_profile_path
+	# echo "alias run='/usr/local/bin/run'" >> bash_profile_path
+	# source bash_profile_path
+}
+
+# Check if "run" is installed correctly
+is_install_success() {
+	echo
+}
+
+# Current install status and version check
+# $1 Current version, $2 New version
+check_current_install() {
+	if [ "$1" == "$2" ]; then
+		echo "Same version $1 is already installed, exiting ..."
+		exit
+	fi
 }
 
 update_config() {
 	current_time=$(date +"%r %a %d %h %y")
 	touch .runconfig_tmp
-	echo "version='$new_version'" >> .runconfig_tmp
-	echo "last_time_updated=$current_time" >> .runconfig_tmp
+	echo "version='$new_version'" > .runconfig_tmp
+	echo "last_time_updated='$current_time'" >> .runconfig_tmp
+	echo "install_path=$run_path" >> .runconfig_tmp
+	echo "config_path=$config_path" >> .runconfig_tmp
+	cp .runconfig_tmp ~/.runconfig
 }
 
 if [ -e "$run_path" ]; then
-	if [ -e "config_path" ]; then
-	# Current install status and version check
-		source "~/.runconfig"
-		if [ "$version" == "$new_version" ]; then
-			echo "Same version $version is already installed, exiting ..."
-			exit
-		fi
-		echo "Current version $version is installed, new version $new_version"''
+	if [ -e "$config_path" ]; then
+		source $config_path
+		check_current_install $version $new_version
+
+		echo "Current version $version , new version $new_version"''
 		install_RUN
 		update_config
 	else
@@ -57,4 +71,3 @@ else
 		exit
 	fi
 fi 
-
